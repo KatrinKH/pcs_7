7fdd771a3b12ef9394ffdf022783a19d8027f9d1
Ekaterina Khomik <Normik2605@gmail.com>
2024-10-30 17:09:34 +0300

    Карточка товара

--- lib/components/item_note.dart
+++ lib/components/item_note.dart
@@ -1,74 +1,70 @@
 import 'package:flutter/material.dart';
-import 'package:pcs_7/model/note.dart'; 
+import 'package:pcs_7/model/note.dart';
 
 class ProductCard extends StatelessWidget {
-  final Analysis product; 
+  final Analysis product;
 
   ProductCard({required this.product});
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Card(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0), 
-          ),
-          elevation: 2,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0), 
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return Card(
+      color: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      elevation: 2,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              product.title,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+              softWrap: true,
+            ),
+            SizedBox(height: 8),
+            Text(
+              product.duration,
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.grey,
+              ),
+            ),
+            SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(
-                  product.title,
-                  style: TextStyle(
-                    fontSize: 16,
-                  ),
-                  softWrap: true 
-                ),
-                SizedBox(height: 8),
-                Text(
-                  product.duration,
-                  style: TextStyle(
-                    fontSize: 14, 
-                    color: Colors.grey,
-                  ),
-                ),
-                SizedBox(height: 8),
                 Text(
                   '${product.price} Р',
                   style: TextStyle(
-                    fontSize: 17, 
+                    fontSize: 17,
                     fontWeight: FontWeight.w500,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Добавить',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromRGBO(26, 111, 238, 1),
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
                   ),
                 ),
               ],
             ),
-          ),
+            SizedBox(height: 1), 
+          ],
         ),
-        Positioned(
-          bottom: 16.0, // расстояние от нижнего края
-          right: 16.0, // расстояние от правого края
-          child: ElevatedButton(
-            onPressed: () {},
-            child: Text(
-              'Добавить', 
-              style: TextStyle(
-                fontSize: 14
-              )
-            ),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: const Color.fromRGBO(26, 111, 238, 1), 
-              foregroundColor: Colors.white,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0), // Минимальное закругление углов
-              ),
-            ),
-          ),
-        ),
-      ],
+      ),
     );
   }
 }
-
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,19 +7,20 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Каталог услуг'),
+        title: Text('Каталог услуг', style: TextStyle(color: Colors.black)),
         backgroundColor: Colors.white,
         elevation: 0,
+        iconTheme: IconThemeData(color: Colors.black), 
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: ListView.builder(
-          itemCount: analyses.length, 
+          itemCount: analyses.length,
           itemBuilder: (context, index) {
-            final product = analyses[index]; 
+            final product = analyses[index];
             return Padding(
-              padding: const EdgeInsets.only(bottom: 16.0), 
-              child: ProductCard(product: product), 
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: ProductCard(product: product),
             );
           },
         ),
